d603665dffd60e144c29e28b7a7f2ab227c51102
yashaswini13695 <yashaswini13695@gmail.com>
2024-03-04 23:12:20 -0500

    Components & nav-bar created for onboarding project
    Components & nav-bar created for onboarding project

--- my-project-v16/src/app/app.module.ts
+++ my-project-v16/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ForgotPasswordComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
