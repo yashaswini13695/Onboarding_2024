093f02135aef4f886a630940ef06e9f600f1054e
yashaswini13695 <yashaswini13695@gmail.com>
2024-03-13 14:35:07 -0400

    onboarding app changes
    onboarding app changes

--- my-project-v16/src/app/app-routing.module.ts
+++ my-project-v16/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { HomeComponent } from './home/home.component';
+import { MyInfoComponent } from './my-info/my-info.component';
+
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path : 'forgot-password', component: ForgotPasswordComponent},
   { path : 'home', component : HomeComponent},
+  { path : 'my-info', component : MyInfoComponent},
+  { path: '**', component: HomeComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
--- my-project-v16/src/app/app.module.ts
+++ my-project-v16/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { SignupComponent } from './signup/signup.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MaterialModule } from "./_material/material.module";
+import { MyInfoComponent } from './my-info/my-info.component';
 
 @NgModule({
   declarations: [
@@ -16,10 +18,12 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     SignupComponent,
     ForgotPasswordComponent,
     HomeComponent,
-    NavBarComponent
+    NavBarComponent,
+    MyInfoComponent
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
