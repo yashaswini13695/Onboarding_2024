b0c7747b755509b14912bacf9f05a41ef00571e1
yashaswini13695 <yashaswini13695@gmail.com>
2024-04-01 23:45:14 -0400

    Changes on Files nav-bar
    Changes on Files nav-bar

--- my-project-v16/src/app/files/files.component.ts
+++ my-project-v16/src/app/files/files.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-files',
   templateUrl: './files.component.html',
   styleUrls: ['./files.component.scss']
 })
-export class FilesComponent {
+export class FilesComponent implements OnInit {
+  documents: any[] = [
+    { name: 'Resume', url: '../../assets/Yashaswini_NG_Resumen_2024.pdf' },
+    // Add more documents as needed
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
