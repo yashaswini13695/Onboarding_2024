2eada9621c88c4e7e893dfe510126e5f00839e0a
yashaswini13695 <71229948+yashaswini13695@users.noreply.github.com>
2024-04-03 22:09:29 -0400

    Revert "Changes on logout and files component to display documents as pdf"

--- my-project-v16/src/app/nav-bar/nav-bar.component.ts
+++ my-project-v16/src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,18 +6,5 @@ import { AuthService } from '../_services/auth.service';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent {
-  currentUserRole: any = '';
-
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {
-    this.authService.getCurrentUserRole().subscribe(role => {
-      this.currentUserRole = role;
-    });
-  }
-  logout() {
-    // Call the logout method from the AuthService
-    this.authService.logout();
-  }
 
 }
