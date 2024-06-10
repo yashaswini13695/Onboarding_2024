9df6c68694bfe72ea75040da2d54c6383428ded1
yashaswini13695 <yashaswini13695@gmail.com>
2024-06-09 20:09:30 -0400

    Node.js integrated with mongodb
    Node.js integrated with mongodb database and getting the details from backend database

---
+++ api/index.js
@@ -0,0 +1,23 @@
+var Express = require("express");
+var MongoClient = require("mongodb").MongoClient;
+var cors = require("cors");
+const multer = require("multer");
+
+var app = Express();
+app.use(cors());
+
+var CONNECTION_STRING = "mongodb+srv://yashaswini13695:Jdk6Jbxrx95yVtx6@onboarding.8iyfepm.mongodb.net/?retryWrites=true&w=majority&appName=Onboarding";
+
+var DATABASENAME="onboarding";
+var database;
+
+app.listen(3000,() => {
+    MongoClient.connect(CONNECTION_STRING,(error, client) => {
+        if (error) {
+            console.error("Failed to connect to MongoDB:", error);
+            return;
+        }
+        database = client.db(DATABASENAME);
+        console.log("MongoDB Connection Successful");
+    })
+})
