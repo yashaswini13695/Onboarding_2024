4acc7e32e01c8d56e12efc47843ed17b9aa43992
yashaswini13695 <yashaswini13695@gmail.com>
2024-06-25 00:23:49 -0400

    Added business logic in service file where user data will be added
    Added business logic in service file where user data will be added

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,73 +1,37 @@
-const userService = require('../services/userService');
-const User = require('../models/userModel');
-const bcrypt = require('bcryptjs');
+const userService = require("../services/userService");
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
 
 exports.getProfile = async (req, res) => {
-    try {
-        const userId = req.user.id; // Assuming the authMiddleware attaches the user object to the request
-        const user = await userService.getUserById(userId);
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+  try {
+    const userId = req.user.id; // Assuming the authMiddleware attaches the user object to the request
+    const user = await userService.getUserById(userId);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 exports.updateProfile = async (req, res) => {
-    try {
-        const userId = req.user.id;
-        const updatedData = req.body;
-        const updatedUser = await userService.updateUser(userId, updatedData);
-        res.status(200).json(updatedUser);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+  try {
+    const userId = req.user.id;
+    const updatedData = req.body;
+    const updatedUser = await userService.updateUser(userId, updatedData);
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 const createUser = async (req, res) => {
-    try {
-        const {
-            firstName,
-            middleName,
-            lastName,
-            email,
-            password,
-            role,
-            department,
-            dateOfBirth,
-            gender,
-            contact,
-            emergencyContact,
-            employmentStatus
-        } = req.body;
-
-        // Check if email ends with @onboarding.com
-        if (!email.endsWith('@onboarding.com')) {
-            return res.status(400).json({ error: 'Email must end with @onboarding.com' });
-        }
-
-        // Create a new user instance
-        const newUser = new User({
-            firstName,
-            middleName,
-            lastName,
-            fullName: `${firstName} ${lastName}`,
-            email,
-            password,
-            role,
-            department,
-            dateOfBirth,
-            gender,
-            contact,
-            emergencyContact,
-            employmentStatus
-        });
-        const savedUser = await newUser.save();
-        res.status(201).json({ message: 'User added successfully', savedUser});
-    } catch (err) {
-        res.status(400).json({ error: err.message });
-    }
+  try {
+    const newUser = await userService.createUser(req.body);
+    res.status(201).json({ message: "User added successfully", newUser });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 module.exports = {
-    createUser
+  createUser,
 };
--- api/services/userService.js
+++ api/services/userService.js
@@ -1,14 +1,61 @@
-const { getDB } = require('../config/db');
+const { getDB } = require("../config/db");
+const User = require('../models/userModel');
 
 const getUserById = async (userId) => {
-    const db = getDB();
-    return await db.collection('userList').findOne({ _id: userId });
+  const db = getDB();
+  return await db.collection("userList").findOne({ _id: userId });
 };
 
 const updateUser = async (userId, updatedData) => {
-    const db = getDB();
-    await db.collection('userList').updateOne({ _id: userId }, { $set: updatedData });
-    return await getUserById(userId);
+  const db = getDB();
+  await db
+    .collection("userList")
+    .updateOne({ _id: userId }, { $set: updatedData });
+  return await getUserById(userId);
 };
 
-module.exports = { getUserById, updateUser };
+const createUser = async (userData) => {
+  // const hashedPassword = await bcrypt.hash(reqBody.password, 10);
+  const {
+    firstName,
+    middleName,
+    lastName,
+    email,
+    password,
+    role,
+    department,
+    dateOfBirth,
+    gender,
+    contact,
+    emergencyContact,
+    employmentStatus,
+  } = userData;
+
+  // Check if email ends with @onboarding.com
+  if (!email.endsWith("@onboarding.com")) {
+    return res
+      .status(400)
+      .json({ error: "Email must end with @onboarding.com" });
+  }
+
+  // Create a new user instance
+  const newUser = new User({
+    firstName,
+    middleName,
+    lastName,
+    fullName: `${firstName} ${lastName}`,
+    email,
+    password,
+    role,
+    department,
+    dateOfBirth,
+    gender,
+    contact,
+    emergencyContact,
+    employmentStatus,
+  });
+  const savedUser = await newUser.save();
+  return savedUser;
+};
+
+module.exports = { getUserById, updateUser, createUser };
